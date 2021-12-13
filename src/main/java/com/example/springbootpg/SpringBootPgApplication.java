c516d250f62812a11c8b4502494f247b24e65486
leafriend <leafriend@gmail.com>
2021-12-14 00:32:14 +0900

    Init Spring Boot project

---
+++ src/main/java/com/example/springbootpg/SpringBootPgApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootpg/SpringBootPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpg;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
