cf2c9abd8757ddce1ad6e41dd37ca1c2136a7a75
leafriend <leafriend@gmail.com>
2024-05-13 23:33:11 +0900

    Define prefix for v1 package

---
+++ src/main/java/com/example/springbootpg/PathPrefixConfig.java
@@ -0,0 +1,17 @@
+package com.example.springbootpg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PathPrefixConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configurePathMatch(@NonNull PathMatchConfigurer configurer) {
+		configurer.addPathPrefix("/v1", HandlerTypePredicate.forBasePackage("com.example.springbootpg.v1.web"));
+	}
+
+}
