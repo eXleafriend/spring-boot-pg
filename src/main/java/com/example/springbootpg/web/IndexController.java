ff190e40818b39f6b76c9125f8f65c224fad929c
leafriend <leafriend@gmail.com>
2024-05-13 23:06:36 +0900

    Add OpenAPI spec on IndexController

--- src/main/java/com/example/springbootpg/web/IndexController.java
+++ src/main/java/com/example/springbootpg/web/IndexController.java
@@ -3,14 +3,24 @@ package com.example.springbootpg.web;
 import java.util.Date;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
 public class IndexController {
 
 	@GetMapping
+	@Operation(
+		summary = "Default API",
+		description = """
+				Returns map with current epoch time in millisecond.
+
+				The map contains only `"epochTime"` entry.
+				"""
+	)
 	@ResponseBody
 	public Map<String, Object> index() {
 		return Map.of("epochTime", new Date().getTime());
