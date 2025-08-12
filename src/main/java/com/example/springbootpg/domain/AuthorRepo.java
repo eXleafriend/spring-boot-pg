2ad005369f591fcab62e292cc6d972ad9ecf806e
leafriend <leafriend@gmail.com>
2025-08-12 23:53:50 +0900

    Support Specification in repos

--- src/main/java/com/example/springbootpg/domain/AuthorRepo.java
+++ src/main/java/com/example/springbootpg/domain/AuthorRepo.java
@@ -1,7 +1,8 @@
 package com.example.springbootpg.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AuthorRepo extends JpaRepository<Author, Long> {
+public interface AuthorRepo extends JpaRepository<Author, Long>, JpaSpecificationExecutor<Author> {
 
 }
--- src/main/java/com/example/springbootpg/domain/PostRepo.java
+++ src/main/java/com/example/springbootpg/domain/PostRepo.java
@@ -1,7 +1,8 @@
 package com.example.springbootpg.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PostRepo extends JpaRepository<Post, Long> {
+public interface PostRepo extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
 }
