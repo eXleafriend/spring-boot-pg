5deaabe43177fbda2f8239b6d242f9781813b1a4
leafriend <leafriend@gmail.com>
2025-08-14 00:03:39 +0900

    SImplify params of list() of controller - Spec & Pageable
    by replacement of parameters
    with @ModelAttribute implementing Specification

--- src/main/java/com/example/springbootpg/web/PostController.java
+++ src/main/java/com/example/springbootpg/web/PostController.java
@@ -2,17 +2,16 @@ package com.example.springbootpg.web;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -29,23 +28,7 @@ public class PostController {
 	private final PostRepo postRepo;
 
 	@GetMapping("")
-	public Page<Post> listPosts(@RequestParam(required = false) final String title,
-			@RequestParam(name = "author.name", required = false) final String authorName,
-			@PageableDefault(10) final Pageable pageable) {
-
-		Specification<Post> spec = Specification.unrestricted();
-
-		if (title != null && !title.isEmpty()) {
-			spec = spec.and((root, query, cb) -> {
-				return cb.like(root.get("title"), "%" + title + "%");
-			});
-		}
-
-		if (authorName != null && !authorName.isEmpty()) {
-			spec = spec.and((root, query, cb) -> {
-				return cb.like(root.get("author").get("name"), "%" + title + "%");
-			});
-		}
+	public Page<Post> listPosts(@ModelAttribute final PostSpec spec, @PageableDefault(10) final Pageable pageable) {
 
 		final var page = postRepo.findAll(spec, pageable);
 		return page;
